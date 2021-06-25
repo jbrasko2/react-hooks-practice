5099c1b0d0945cba89de839e2e3d487c4a47259d
jbrasko <jbrasko0508@gmail.com>
2021-06-24 17:57:13 -0700

    create useForm hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React, { useState } from 'react';
+import { useForm } from './useForm';
 
 function App() {
-  const [{ count, count2 }, setCount] = useState({ count: 10, count2: 20 });
+  const [values, handleChange] = useForm({ email: '', password: '' });
 
   return (
     <div>
-      {/*use updater function (currentCount) rather than setCount(count + 1) to avoid overwriting*/}
-      <button
-        onClick={() =>
-          setCount(currentState => ({
-            ...currentState,
-            count: currentState.count + 1,
-          }))
-        }
-      >
-        +
-      </button>
-      <div>count 1: {count}</div>
-      <div>count 2: {count2}</div>
+      <input name='email' value={values.email} onChange={handleChange} />
+      <input
+        type='password'
+        name='password'
+        value={values.password}
+        onChange={handleChange}
+      />
     </div>
   );
 }
