f0d8657a5afc825922e2f06a98faaedbeed555f1
jbrasko <jbrasko0508@gmail.com>
2021-06-24 17:31:44 -0700

    create count button, destructure count state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(10)
+  const [{ count, count2 }, setCount] = useState({ count: 10, count2: 20 });
 
   return (
     <div>
-      <button onClick={() => setCount(count + 1)}>+</button>
-      <div>{count}</div>
+      {/*use updater function (currentCount) rather than setCount(count + 1) to avoid overwriting*/}
+      <button
+        onClick={() =>
+          setCount(currentState => ({
+            ...currentState,
+            count: currentState.count + 1,
+          }))
+        }
+      >
+        +
+      </button>
+      <div>count 1: {count}</div>
+      <div>count 2: {count2}</div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
